f15b848f5063a00aa5abec0faa8bf116417a0cdd
nyk <drivingdeveloper@gmail.com>
2021-04-11 13:37:17 +0900

    #send information from a componentn to another component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React from 'react' //추가
-import Potato from './Potato';
+
+/*
+function Food(props)
+property 가 object로 넘어와서 console에는 fav = "kimchi" 라고 적힘
+function Food({fav}) {}
+{프로퍼티} 라고 써주면 프로퍼티 안의 값을 바로 가져올 수 있음
+*/
+
+function Food({fav}) {
+  return <h1>I Love {fav}</h1>
+}
+
+// component 는 대문자로 시작해야 한다.
+// jsx : component 에 정보를 보낼 수 있다
+// 그렇기 때문에 react는 재사용 가능한 component 를 계속 만들 수 있다.
+// component에서 component로 children component로 정보를 보내는 방법!
+// app component 에서 food component로 정보 보내는 법
+// <Food name = "kimchi"/> = > gave food component a property(prop) a name with kimchi
+// <Food name = "kimchi" something = {true} papapapapa = {["hello", 1,2,3,4, true]}/> 
 
 function App() {
   return (
     <div>
       <h1>Hello</h1>
-      <Potato />
+      <Food fav="kimchi"/> 
+      <Food fav="ramen"/> 
+      <Food fav="samgiopsal"/> 
+      <Food fav="chukumi"/> 
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -5,7 +5,11 @@ import App from './App';
 ReactDOM.render(<App />, document.getElementById('root'));
 //<App /> : component == function that returns HTML
 // component 가 작동하려면 <App /> 처럼 HTML 형태에있어야 REACT가 COMPONENT로 인식함
+// component 는 대문자로 시작해야 한다.
 // REACT 만의 개념 : JSX : JAVASCRIPT + HTML
+// jsx는 component 에 정보를 보낼 수 있다
+// 그렇기 때문에 react는 재사용 가능한 component 를 계속 만들 수 있다.
+// jsx 쓰는 법 : 속성 = " "
 
 // react application 이 하나의 component 만을 rendering 해야하기 때문에 아래 소스는 실행 오류
 /*
