c5791b8aa3dd6fbf4d4a808a88d466b9aefd6fbd
nyk <drivingdeveloper@gmail.com>
2021-04-13 14:08:08 +0900

    #add comment

--- src/App.js
+++ src/App.js
@@ -11,7 +11,9 @@ import PropTypes from "prop-types"
   매번 state의 상태를 변경할 때 react가 reder function을 호출하여 refresh 해주려면
   this.state.count 대신 this.setState({count:1})하면, react는 state를 refresh하고 또한 render function을 호출하도록 사용
 
-  setState 로 새로운 state 를 render 한다.
+  ★ setState 로 새로운 state 를 render 한다.
+
+  this.setState(current => ({ count: current.count + 1 })); 이라 하므로써 class attribute의 '현재 상태' 사용
 
   class는 state 를 가지고 있고, state 는 object임.
   state object는 component의 data를 넣을 공간이 있고 이 데이터는 변할 수 있음
