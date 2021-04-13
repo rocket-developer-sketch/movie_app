ece360c063a112316b53c80f43b634a7b6e11fc3
nyk <drivingdeveloper@gmail.com>
2021-04-13 14:00:55 +0900

    #setState() make new state with render() and class attribute refresh(with no good way)

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import PropTypes from "prop-types"
   매번 state의 상태를 변경할 때 react가 reder function을 호출하여 refresh 해주려면
   this.state.count 대신 this.setState({count:1})하면, react는 state를 refresh하고 또한 render function을 호출하도록 사용
 
+  setState 로 새로운 state 를 render 한다.
 
   class는 state 를 가지고 있고, state 는 object임.
   state object는 component의 data를 넣을 공간이 있고 이 데이터는 변할 수 있음
@@ -22,11 +23,11 @@ class App extends React.Component{
   };
 
   add = () => {
-    this.setState({ count: 1 });
+    this.setState({ count: this.state.count + 1 });
   };
-  
+
   minus = () => {
-    this.setState({ count: -1 });
+    this.setState({ count: this.state.count - 1 });
   };
 
   render () {
