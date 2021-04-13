08c98e3b58730e72dc53be4c459af614d2dc668d
nyk <drivingdeveloper@gmail.com>
2021-04-13 18:17:37 +0900

    #add Navigation(move from a page to another)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React from "react";
 import { HashRouter, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import About from "./routes/About";
+import Navigation from "./components/Navigation";
+// HashRouter 를 사용하면 url /#/ 이 붙음. BrowserRouter 은 붙지 않는 다는 차이점이 있음
+// Browser Router는 github pages에 올리기 까다로움
+
 /* 라우터 역할 js */
 /// <Route> : 렌더링할 스크린 과 무엇을 해야할 지 정해주기
 //  <Route path="/about/" component={About} />
@@ -11,14 +15,17 @@ import About from "./routes/About";
 // /home 에 매치가 2개 되기 때문에
 // /home 에서 동작하는 것 + /introduction 에서 동작하는 것
 // 2개의 component를 가지고 모두가 한 화면에 보여짐
+// 해결 : exact = {true} 속성을 붙여주므로써 완전 동일한 path만 router 가 렌더링하도록 함
 
-// exact = {true} 속성을 붙여주므로써 완전 동일한 path만 router 가 렌더링하도록 함
+// link를 가진 component Navigation을 Route 밖으로 내놓으면 동작하지 않음!
 function App() {
-  return <HashRouter>
+  return (
+  <HashRouter>
+    <Navigation />
     <Route path="/" exact={true} component={Home} />
     <Route path="/about" component={About} />
   </HashRouter>
-
+  )
 };
 
 export default App;
