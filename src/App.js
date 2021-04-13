cb3c91c4ec44ae4b2de12e8c1470b09230700665
nyk <drivingdeveloper@gmail.com>
2021-04-13 15:21:48 +0900

    #sort data by rating

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ class App extends React.Component{
   };
 
   getMovies = async() => {
-    const {data: { data: { movies } } } = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    const {data: { data: { movies } } } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
     console.log(movies);
     this.setState({ movies, isLoading: false });
   }
