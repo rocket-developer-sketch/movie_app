686215da4b52de4928c24cc48c379a513d104031
nyk <drivingdeveloper@gmail.com>
2021-04-13 16:18:28 +0900

    #listing genre using map() ( key is an argument from map() ) after validation of array with propTypes

--- src/Movie.js
+++ src/Movie.js
@@ -14,7 +14,13 @@ function Movie({year, title, summary, poster, genres}) {
             <div className="movie__data">
                 <h3 className="movie__title">{title}</h3>
                 <h5 className="movie__year">{year}</h5>
+                <ul className="genres"> 
+                    {genres.map((genre, index) => (
+                        <li className="genres__genre" key={index}>{genre}</li>
+                    ))}
+                </ul>
                 <p className="movie__summary">{summary}</p>
+          
             </div>
         </div>
     </div>;
