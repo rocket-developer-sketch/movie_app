20f77e247d0954b949a5fad5b9100f94ecf29d44
nyk <drivingdeveloper@gmail.com>
2021-04-13 12:33:06 +0900

    #how to refresh render method with setState()

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ import PropTypes from "prop-types"
   function은 무언가를 return하고 screen에 표시됨.
   class component는 class지만 react component로 부터 확장되고 screen에 표시 됨
   react 는 자동으로 render method 실행 함
+  react는 reder를 refresh 해 주지 않음. 
+
+  매번 state의 상태를 변경할 때 react가 reder function을 호출하여 refresh 해주려면
+  this.state.count 대신 this.setState({count:1})하면, react는 state를 refresh하고 또한 render function을 호출하도록 사용
+
 
   class는 state 를 가지고 있고, state 는 object임.
   state object는 component의 data를 넣을 공간이 있고 이 데이터는 변할 수 있음
@@ -17,13 +22,14 @@ class App extends React.Component{
   };
 
   add = () => {
-    console.log("function add");
-  }
+    this.setState({ count: 1 });
+  };
+  
   minus = () => {
-    console.log("function minus");
-  }
+    this.setState({ count: -1 });
+  };
 
-  render (){
+  render () {
     return ( 
     <div>
       <h1>The number is {this.state.count}</h1>
